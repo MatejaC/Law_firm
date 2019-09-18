3394df39b510cfb9b06fa6780e4ef484e9bf4909
Mateja Cimpersak <matejacimpersak@gmail.com>
2019-09-18 14:16:08 +0200

    finished sticky navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,27 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	$('.js--pov-box').waypoint(
+		function(direction) {
+			if (direction == 'down') {
+				$('nav').addClass('sticky');
+			} else {
+				$('nav').removeClass('sticky');
+			}
+		},
+		{
+			offset: '105px'
+		}
+	);
+
+	$('.js--section-prakse').waypoint(function(direction) {
+		if (direction == 'down') {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	});
+});
+/* var waypoints = $('#handler-first').waypoint(function (direction) {
+  notify(this.element.id + ' hit 25% from top of window')
+ }, {
+  offset: '25%'
+ }) */
